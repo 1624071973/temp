eb070755907d7a10319cbe803b4a38595407942e
taoli <1624071973@qq.com>
2019-04-19 23:39:00 +0800

    3

---
+++ file/demo.vue
@@ -0,0 +1,49 @@
+<template>
+	<Tree :data="data3" :load-data="loadData" show-checkbox></Tree>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			layerList: [
+				{
+					id: '001',
+					name: 'layer01'
+				},
+				{
+					id: '002',
+					name: 'layer02'
+				}
+			],
+
+			data3: [
+				{
+					title: 'parent',
+					loading: false,
+					id:'000',
+					children: []
+				}
+			]
+		};
+	},
+	methods: {
+		loadData(item, callback) {
+			const data = this.query(item);
+			console.log(item);
+			callback(data);
+		},
+
+		query(id) {
+			var layers = [];
+			this.layerList.forEach(i => {
+				layers.push({
+					title: i.name,
+					loading: false,
+					children: []
+				});
+			});
+			return layers;
+		}
+	}
+};
+</script>
