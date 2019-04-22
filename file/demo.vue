ce412ec58d5888301984edad42cadbbe57a395d1
taoli <1624071973@qq.com>
2019-04-23 01:56:36 +0800

    3

--- file/demo.vue
+++ file/demo.vue
@@ -1,49 +1,114 @@
 <template>
-	<Tree :data="data3" :load-data="loadData" show-checkbox></Tree>
+	<div style="width:600px">
+		<Tree :data="data5" :render="renderContent"></Tree>
+	</div>
+
 </template>
 <script>
-export default {
-	data() {
-		return {
-			layerList: [
-				{
-					id: '001',
-					name: 'layer01'
-				},
-				{
-					id: '002',
-					name: 'layer02'
-				}
-			],
+	export default {
+		data() {
+			return {
+				data5: [
+					{
+						title: 'parent 1',
+						id:'001',
+						expand: false,
+						render: (h, {root, node, data}) => {
+							return h('span', {
+								style: {
+									display: 'inline-block',
+									width: '100%'
+								}
+							}, [
+								h('span',{
+									on: {
+										click: () => {
+											this.queryList(data)
+										}
+									}
+								} ,[
+									h('span', data.title)
+								]),
+								h('span', {
+									style: {
+										display: 'inline-block',
+										float: 'right',
+										marginRight: '32px'
+									},
 
-			data3: [
-				{
-					title: 'parent',
-					loading: false,
-					id:'000',
-					children: []
+								})
+							]);
+						},
+						children: [{}]
+					}
+				],
+				buttonProps: {
+					type: 'default',
+					size: 'small',
 				}
-			]
-		};
-	},
-	methods: {
-		loadData(item, callback) {
-			const data = this.query(item);
-			console.log(item);
-			callback(data);
+			}
 		},
+		methods: {
+			renderContent(h, {root, node, data}) {
+				return h('span', {
+					style: {
+						display: 'inline-block',
+						width: '100%'
+					}
+				}, [
+					h('span',{
+						on: {
+							click: () => {
+								this.queryList(data)
+							}
+						}
+					}, [
+						h('span', data.title)
+					]),
+					h('span', {
+						style: {
+							display: 'inline-block',
+							float: 'right',
+							marginRight: '32px'
+						}
+					}, [
+						h('Button', {
+							props: Object.assign({}, this.buttonProps, {
+								icon: 'ios-add'
+							}),
+							style: {
+								marginRight: '8px'
+							},
+							on: {
+								click: () => {
+									alert(data.title)
+								}
+							}
+						})
+					])
+				]);
+			},
+			queryList(data) {
+				//const children = data.children || [];
+				console.log(data);
+				let children=[{
+					title: 'appended node'+data.id,
+					expand: false,
+					id: data.id+'1',
+					children: [{}]
+				}, {
+					title: 'child 1-1'+data.id,
+					expand: false,
+					id: data.id+'02',
+					children: [{}]
+				}];
+				if(data.id=='00111'){
+					alert('555');
+					this.$set(data, 'disabled', true);
+				}
 
-		query(id) {
-			var layers = [];
-			this.layerList.forEach(i => {
-				layers.push({
-					title: i.name,
-					loading: false,
-					children: []
-				});
-			});
-			return layers;
+				this.$set(data, 'children', children);
+			}
 		}
 	}
-};
 </script>
